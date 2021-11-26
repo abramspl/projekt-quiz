76f76496e0107a1fbd167e590900d7164a10f2e9
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2021-11-26 14:54:52 +0100

    przygotowanie podstrony select.html, zmiana nazwy klasy HelloController na FrontendController

--- src/main/java/pl/abrams/quiz/controllers/FrontendController.java
+++ src/main/java/pl/abrams/quiz/controllers/FrontendController.java
@@ -5,11 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
+public class FrontendController {
 
     @GetMapping("/")
     public String hello(Model model) {
         model.addAttribute("message", "jakaś wiadomość");
         return "index";
+    }
+
+    @GetMapping("/select")
+    public String select(Model model){
+        return "select";
     }
 }
