5956404e5eeed2418186e3b3fa9f9be17a7ff908
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2021-11-29 11:59:50 +0100

    wypełnienie podstrony select.html treścią, podawanie liczby pytań i wysłanie formularza

---
+++ src/main/java/pl/abrams/quiz/frontend/FrontendController.java
@@ -0,0 +1,31 @@
+package pl.abrams.quiz.frontend;
+
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Log
+public class FrontendController {
+
+    @GetMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("message", "jakaś wiadomość");
+        return "index";
+    }
+
+    @GetMapping("/select")
+    public String select(Model model){
+        model.addAttribute("gameOptions", new GameOptions());
+        return "select";
+    }
+
+    @PostMapping("/select")
+    public String postSelectForm(Model model, @ModelAttribute GameOptions gameOptions){
+        log.info("Został przesłany formularz z danymi: " + gameOptions);
+        return "index";
+    }
+}
---
+++ src/main/java/pl/abrams/quiz/frontend/GameOptions.java
@@ -0,0 +1,8 @@
+package pl.abrams.quiz.frontend;
+
+import lombok.Data;
+
+@Data
+public class GameOptions {
+    private int numberOfQuestions = 5;
+}
