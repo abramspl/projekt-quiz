79229f96fc35eccc33c0fd011d30cd321d384414
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2021-11-30 11:30:46 +0100

    przygotowanie klasy OngoingGameService która przechowuje bieżący stan gry

--- src/main/java/pl/abrams/quiz/services/QuizDataService.java
+++ src/main/java/pl/abrams/quiz/services/QuizDataService.java
@@ -6,6 +6,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import pl.abrams.quiz.QuestionsDto;
 import pl.abrams.quiz.dto.CategoriesDto;
+import pl.abrams.quiz.frontend.GameOptions;
 
 import java.net.URI;
 import java.util.List;
@@ -21,17 +22,18 @@ public class QuizDataService {
         return result.getCategories();
     }
 
-    public void getQuizQuestions() {
+    public List<QuestionsDto.QuestionDto> getQuizQuestions(GameOptions gameOptions) {
         RestTemplate restTemplate = new RestTemplate();
 
         URI uri = UriComponentsBuilder.fromHttpUrl("https://opentdb.com/api.php")
-                .queryParam("amount", 2)
-                .queryParam("category", 25)
-                .queryParam("difficulty", "medium")
+                .queryParam("amount", gameOptions.getNumberOfQuestions())
+                .queryParam("category", gameOptions.getCategoryId())
+                .queryParam("difficulty", gameOptions.getDifficulty())
                 .build().toUri();
         log.info("Pytanie quizu — pobierz URL: " + uri);
 
         QuestionsDto result = restTemplate.getForObject(uri, QuestionsDto.class);
         log.info("Pytania quizowe: " + result.getResults());
+        return result.getResults();
     }
 }
