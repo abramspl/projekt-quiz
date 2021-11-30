013aa4fb00c417af9dc5a671d7a7aebdfacf04ca
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2021-11-30 13:39:21 +0100

    wiele gier w tym samym czasie - dodanie adnotacji @SessionScope nad klasą OngoingGameService

--- src/main/java/pl/abrams/quiz/services/OngoingGameService.java
+++ src/main/java/pl/abrams/quiz/services/OngoingGameService.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 import pl.abrams.quiz.QuestionsDto;
 import pl.abrams.quiz.frontend.Difficulty;
 import pl.abrams.quiz.frontend.GameOptions;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@SessionScope
 @Log
 public class OngoingGameService {
     private GameOptions gameOptions;
