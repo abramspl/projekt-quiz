0bc01bfdcf581f2a9d7c7d9f3b1802daec1e4a8b
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2021-11-30 11:46:42 +0100

    przygotowanie frontendu

--- src/main/java/pl/abrams/quiz/frontend/FrontendController.java
+++ src/main/java/pl/abrams/quiz/frontend/FrontendController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import pl.abrams.quiz.services.OngoingGameService;
 import pl.abrams.quiz.services.QuizDataService;
 
 @Controller
@@ -16,6 +17,9 @@ public class FrontendController {
     @Autowired
     private QuizDataService quizDataService;
 
+    @Autowired
+    private OngoingGameService ongoingGameService;
+
     @GetMapping("/")
     public String hello(Model model) {
         model.addAttribute("message", "jakaś wiadomość");
@@ -23,15 +27,26 @@ public class FrontendController {
     }
 
     @GetMapping("/select")
-    public String select(Model model){
+    public String select(Model model) {
         model.addAttribute("gameOptions", new GameOptions());
         model.addAttribute("categories", quizDataService.getQuizCategories());
         return "select";
     }
 
     @PostMapping("/select")
-    public String postSelectForm(Model model, @ModelAttribute GameOptions gameOptions){
+    public String postSelectForm(Model model, @ModelAttribute GameOptions gameOptions) {
         log.info("Został przesłany formularz z danymi: " + gameOptions);
-        return "index";
+        ongoingGameService.init(gameOptions);
+        return "redirect:game";
+    }
+
+    @GetMapping("/game")
+    public String game(Model model){
+        model.addAttribute("userAnswer", new UserAnswer());
+        model.addAttribute("currentQuestionNumber", ongoingGameService.getCurrentQuestionNumber());
+        model.addAttribute("totalQuestionNumber", ongoingGameService.getTotalQuestionNumber());
+        model.addAttribute("currentQuestion", ongoingGameService.getCurrentQuestion());
+        model.addAttribute("currentQuestionAnswers", ongoingGameService.getCurrentQuestionAnswersInRandomOrder());
+        return "game";
     }
 }
